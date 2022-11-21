e85e138bc8d548912019c2026b107b7e85c035e3
Willian Lima <william_slayer@hotmail.com>
2022-11-21 08:37:24 -0300

    criado component dashboard

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,27 @@
+import { HeroModel } from './../shared/hero.model';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../core/hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  heroes: HeroModel[]=[]
+
+  constructor(
+    private heroService: HeroService
+  ) { }
+
+  ngOnInit() {
+    this.getheroes()
+  }
+
+  getheroes(){
+    this.heroService.getHeroes()
+    .subscribe(heros => this.heroes = this.heroes.slice(1,5))
+  }
+
+}
